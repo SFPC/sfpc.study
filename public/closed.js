6119437b83ac6853790245c662073464bc889d55
Zainab Aliyu <zoaliyu@gmail.com>
2022-03-26 21:52:26 -0400

    closed and projects

---
+++ public/closed.js
@@ -0,0 +1,21 @@
+
+// CLOSE WEBSITE ON WEEKEND
+function closed() {
+  var currentTime = new Date();
+  var currentHour = currentTime.getHours();
+  var isWeekend = currentTime.getDay()%6==0;
+
+
+  if ((currentHour < 9) || (currentHour > 21) || isWeekend) {
+    window.location.replace("closed.html");
+  }
+}
+
+var isClosed = window.location.href.indexOf('closed') == -1;
+var notClosed = window.location.href.indexOf('closed') == 1;
+
+if (isClosed) {
+   closed();
+} else {
+  $('.mobile-menu').addClass('hide-menu');
+}
--- public/outside.js
+++ public/outside.js
@@ -30,25 +30,25 @@ if (checkMobile()) {
 
 
 // CLOSE WEBSITE ON WEEKEND
-function closed() {
-  var currentTime = new Date();
-  var currentHour = currentTime.getHours();
-  var isWeekend = currentTime.getDay()%6==0;
-
-
-  if ((currentHour < 9) || (currentHour > 21) || isWeekend) {
-    window.location.replace("closed.html");
-  }
-}
-
-var isClosed = window.location.href.indexOf('closed') == -1;
-var notClosed = window.location.href.indexOf('closed') == 1;
-
-if (isClosed) {
-   closed();
-} else {
-  $('.mobile-menu').addClass('hide-menu');
-}
+// function closed() {
+//   var currentTime = new Date();
+//   var currentHour = currentTime.getHours();
+//   var isWeekend = currentTime.getDay()%6==0;
+//
+//
+//   if ((currentHour < 9) || (currentHour > 21) || isWeekend) {
+//     window.location.replace("closed.html");
+//   }
+// }
+//
+// var isClosed = window.location.href.indexOf('closed') == -1;
+// var notClosed = window.location.href.indexOf('closed') == 1;
+//
+// if (isClosed) {
+//    closed();
+// } else {
+//   $('.mobile-menu').addClass('hide-menu');
+// }
 
 
 
