58482e414ba9683abb589641630bc4e425d9f34e
zai <zai@zais-mbp.myfiosgateway.com>
2022-12-05 00:41:02 -0500

    progress bar

--- public/outside.js
+++ public/outside.js
@@ -388,10 +388,36 @@ $(document).ready(function() {
         var countdown = (Math.ceil((end.getTime()-today.getTime())/(one_day)));
         $('.fr-left h6').html(countdown);
 
-        var percentage = raised / goal * 100;
+        var percentage = (raised / goal * 100).toFixed(0);
+
+
+        function formatMoney(number, decPlaces, decSep, thouSep) {
+              decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
+              decSep = typeof decSep === "undefined" ? "." : decSep;
+              thouSep = typeof thouSep === "undefined" ? "," : thouSep;
+              var sign = number < 0 ? "-" : "";
+              var i = String(parseInt(number = Math.abs(Number(number) || 0).toFixed(decPlaces)));
+              var j = (j = i.length) > 3 ? j % 3 : 0;
+
+              return sign +
+                  (j ? i.substr(0, j) + thouSep : "") +
+                  i.substr(j).replace(/(\decSep{3})(?=\decSep)/g, "$1" + thouSep) +
+                  (decPlaces ? decSep + Math.abs(number - i).toFixed(decPlaces).slice(2) : "");
+          }
+
+
+
+        var goaldollars = formatMoney(goal, 0, ".", ",");
+
         console.log(percentage)
 
-        
+
+        $('.fundraiser-progress h6').html(percentage + '%');
+        $('.fundraiser-progress').css('width', percentage + "%");
+
+        $('.fundraiser-stats .fr-right h6').html('$' + goaldollars);
+
+
 
 
 
