5f384f399d398c236d1416fcf7712d069151f50f
zai <zai@zais-mbp.myfiosgateway.com>
2022-12-06 20:50:54 -0500

    fiscal

--- public/outside.js
+++ public/outside.js
@@ -450,31 +450,38 @@ $(document).ready(function() {
         var percentage = (raised / goal * 100).toFixed(0);
 
 
-        function formatMoney(number, decPlaces, decSep, thouSep) {
-              decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
-              decSep = typeof decSep === "undefined" ? "." : decSep;
-              thouSep = typeof thouSep === "undefined" ? "," : thouSep;
-              var sign = number < 0 ? "-" : "";
-              var i = String(parseInt(number = Math.abs(Number(number) || 0).toFixed(decPlaces)));
-              var j = (j = i.length) > 3 ? j % 3 : 0;
+        const formatter = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
 
-              return sign +
-                  (j ? i.substr(0, j) + thouSep : "") +
-                  i.substr(j).replace(/(\decSep{3})(?=\decSep)/g, "$1" + thouSep) +
-                  (decPlaces ? decSep + Math.abs(number - i).toFixed(decPlaces).slice(2) : "");
-          }
+            // These options are needed to round to whole numbers if that's what you want.
+            minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+            maximumFractionDigits: 1, // (causes 2500.99 to be printed as $2,501)
+          });
 
 
+        //
+        // var goaldollars = formatMoney(goal, 0, ".", ",");
+        // var totaldollars = formatMoney(raised, 0, ".", ",");
 
-        var goaldollars = formatMoney(goal, 0, ".", ",");
 
-        console.log(percentage)
+        var goaldollars = formatter.format(goal);
+        var totaldollars = formatter.format(raised);
 
 
-        $('.fundraiser-progress h6').html(percentage + '%');
+        formatter.format(goal)
+
+
+        if (percentage < 100) {
+           $('.fundraiser-progress h6').html(percentage + '%');
+        } else {
+          $('.fundraiser-progress h6').html(totaldollars);
+        }
+        // $('.fundraiser-progress h6').html(percentage + '%');
+
         $('.fundraiser-progress').css('width', percentage + "%");
 
-        $('.fundraiser-stats .fr-right h6').html('$' + goaldollars);
+        $('.fundraiser-stats .fr-right h6').html(goaldollars);
 
 
         function getRandomArbitrary(min, max) {
