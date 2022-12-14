c54e14c570c2f74098be0aee59ff444da7810b9a
zai <zai@zais-mbp.myfiosgateway.com>
2022-12-13 21:37:46 -0500

    remove anim js

--- public/outside.js
+++ public/outside.js
@@ -353,31 +353,31 @@ $(".hide-content").fadeIn(700);
 
 
 
-
-$(document).ready(function() {
-  $(".animsition").animsition({
-    inClass: 'zoom-in',
-    outClass: 'zoom-out',
-    inDuration: 1500,
-    outDuration: 800,
-    linkElement: '.animsition-link',
-    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-    loading: true,
-    loadingParentElement: 'body', //animsition wrapper element
-    loadingClass: 'animsition-loading',
-    loadingInner: '', // e.g '<img src="loading.svg" />'
-    timeout: false,
-    timeoutCountdown: 5000,
-    onLoadEvent: true,
-    browser: [ 'animation-duration', '-webkit-animation-duration'],
-    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    overlay : false,
-    overlayClass : 'animsition-overlay-slide',
-    overlayParentElement : 'body',
-    transition: function(url){ window.location.href = url; }
-  });
-});
+//
+// $(document).ready(function() {
+//   $(".animsition").animsition({
+//     inClass: 'zoom-in',
+//     outClass: 'zoom-out',
+//     inDuration: 1500,
+//     outDuration: 800,
+//     linkElement: '.animsition-link',
+//     // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+//     loading: true,
+//     loadingParentElement: 'body', //animsition wrapper element
+//     loadingClass: 'animsition-loading',
+//     loadingInner: '', // e.g '<img src="loading.svg" />'
+//     timeout: false,
+//     timeoutCountdown: 5000,
+//     onLoadEvent: true,
+//     browser: [ 'animation-duration', '-webkit-animation-duration'],
+//     // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+//     // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+//     overlay : false,
+//     overlayClass : 'animsition-overlay-slide',
+//     overlayParentElement : 'body',
+//     transition: function(url){ window.location.href = url; }
+//   });
+// });
 
 /////////////////////// DOCU READY //////////////////////////
 // setTimeout("closed()", 1);
@@ -422,7 +422,7 @@ $(document).ready(function() {
         var goal = 60000;
 
         // estimated amount raised
-        var raised = 29633;
+        var raised = 29657;
 
         // end date of fundraiser
         var end = new Date('January 3, 2023');
