5e03ac688e7038bedaab0236205e3b21705298fe
zai <zai@zais-MacBook-Pro.local>
2022-12-19 23:15:27 -0500

    fr

--- public/outside.js
+++ public/outside.js
@@ -422,7 +422,7 @@ $(document).ready(function() {
         var goal = 60000;
 
         // estimated amount raised
-        var raised = 34110;
+        var raised = 34380;
 
         // end date of fundraiser
         var end = new Date('January 3, 2023');
