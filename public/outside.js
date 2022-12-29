264fb1eb0e0a133e6f31f6e3171abaca8b937b03
zai <zai@zais-MacBook-Pro.local>
2022-12-29 15:49:03 -0500

    fr

--- public/outside.js
+++ public/outside.js
@@ -422,7 +422,7 @@ $(document).ready(function() {
         var goal = 60000;
 
         // estimated amount raised
-        var raised = 43426;
+        var raised = 43776;
 
         // end date of fundraiser
         var today = new Date();
