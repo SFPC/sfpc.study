c3457e8cf6101f350f99ad745e62bc9d112ae8f3
zai <zai@zais-mbp.myfiosgateway.com>
2023-01-03 17:31:25 -0500

    fr

--- public/outside.js
+++ public/outside.js
@@ -422,7 +422,7 @@ $(document).ready(function() {
         var goal = 60000;
 
         // estimated amount raised
-        var raised = 90445;
+        var raised = 90695;
 
         // end date of fundraiser
         var today = new Date();
