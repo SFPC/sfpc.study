2dd4ba0f61b72d72ea19cf4541c72d2fc6422b5e
zai <zai@zais-mbp.myfiosgateway.com>
2023-01-03 02:27:39 -0500

    timer

--- public/outside.js
+++ public/outside.js
@@ -443,8 +443,41 @@ $(document).ready(function() {
           return (start - end) / oneDay;
         }
 
+
+
+        var countDownDate = new Date("Jan 3, 2023 23:59:59").getTime();
+
+        // Update the count down every 1 second
+        var x = setInterval(function() {
+
+          // Get today's date and time
+          var now = new Date().getTime();
+
+          // Find the distance between now and the count down date
+          var distance = countDownDate - now;
+
+          // Time calculations for days, hours, minutes and seconds
+          var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+          var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+          var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+          // Display the result in the element with id="demo"
+          $('.fr-left h6').html(hours + "h "
+          + minutes + "m " + seconds + "s ");
+
+          // If the count down is finished, write some text
+          if (distance < 0) {
+            clearInterval(x);
+            // document.getElementById("demo").innerHTML = "EXPIRED";
+            $('.fr-left h6').html("0")
+          }
+        }, 1000);
+
+
+
         var daysLeft = DaysBetween(today, end);
-        $('.fr-left h6').html(daysLeft);
+        // $('.fr-left h6').html(daysLeft);
 
 
         // //goal of the fundraiser, currently $60,000
