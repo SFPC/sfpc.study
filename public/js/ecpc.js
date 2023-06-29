bf0d60e73a2414d6361ddb47944964743b547303
zai <zai@zais-mbp.myfiosgateway.com>
2023-06-29 12:40:13 -0400

    ecpc

---
+++ public/js/ecpc.js
@@ -0,0 +1,33 @@
+var d = new Date();
+console.log(formatDate(d));
+
+function formatDate(d){
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    return days[d.getDay()] + ", " + months[d.getMonth()] + " " + (d.getDate() < 10 ? "0" + d.getDate() : d.getDate()) + ", " + d.getFullYear();
+}
+
+function calcTime(city, offset) {
+    // create Date object for current location
+    var d = new Date();
+
+    // convert to msec
+    // subtract local time zone offset
+    // get UTC time in msec
+    var utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+
+    // create new Date object for different city
+    // using supplied offset
+    var nd = new Date(utc + (3600000*offset));
+
+    // return time as a string
+    return nd.toLocaleString();
+}
+
+
+$(document).ready(function() {
+
+$('#date').html(formatDate(d))
+
+
+});
