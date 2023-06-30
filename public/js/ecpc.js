caaaa56dc961ea09e47fda868b15433b3c835804
zai <zai@zais-mbp.mynetworksettings.com>
2023-06-30 16:14:36 -0400

    ecpc

--- public/js/ecpc.js
+++ public/js/ecpc.js
@@ -1,5 +1,11 @@
+
+
+
+
+
+
+
 var d = new Date();
-console.log(formatDate(d));
 
 function formatDate(d){
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -30,17 +36,89 @@ function calcTime(offset) {
 
 $(document).ready(function() {
 
+
+  $(document).on("click", ".accordionTitle" , function() {
+  		$(this).siblings('.accordionContent').slideToggle();
+  		$(this).toggleClass('contentOpen');
+  	});
+
+
+  var contentAbout = $('#content-about').html();
+  var contentCalendar = $('#content-calendar').html();
+  var contentVisit = $('#content-visit').html();
+  var contentGuestbook = $('#content-guestbook').html();
+  var contentHelp = $('#content-help').html();
+  var contentPrint = $('#content-print').html();
+
+  var lastDiv = $('.terminal:last-child');
+  var lastDivHeight = $(lastDiv).height();
+  var goToLast = $(document).height() - lastDivHeight;
+
+  $(".link-about").click(function() {
+      $(".terminal").append(contentAbout);
+      $("#content-about.load-content").remove();  
+      $("html, body").animate({ scrollTop:$('.terminal .page').last().position().top  }, "slow");
+
+
+
+  });
+
+  $(".link-calendar").click(function() {
+      $(".terminal").append(contentCalendar);
+      $(".hide-content #content-calendar").remove();
+      $("html, body").animate({ scrollTop:$('.terminal .page').last().position().top  }, "slow");
+
+  });
+
+  $(".link-visit").click(function() {
+      $(".terminal").append(contentVisit);
+      $("#content-visit").removeClass("load-content").addClass("show-content");
+      $("html, body").animate({ scrollTop:$('.terminal .page').last().position().top  }, "slow");
+  });
+
+  $(".link-guestbook").click(function() {
+      $(".terminal").append(contentGuestbook);
+      $("#content-guestbook").removeClass("load-content").addClass("show-content");
+      $("html, body").animate({ scrollTop:$('.terminal .page').last().position().top  }, "slow");
+  });
+
+  $(".link-help").click(function() {
+      $(".terminal").append(contentHelp);
+      $("#content-help").removeClass("load-content").addClass("show-content");
+      $("html, body").animate({ scrollTop:$('.terminal .page').last().position().top  }, "slow");
+  });
+
+  $(".link-print").click(function() {
+      $(".terminal").append(contentPrint);
+      $("#content-print").removeClass("load-content").addClass("show-content");
+      $("html, body").animate({ scrollTop:$('.terminal .page').last().position().top  }, "slow");
+  });
+
+
+
 var time = calcTime('0');
 
 $('#date').html(formatDate(d));
 
 
-$('.accordionTitle').click(function(){
-		$(this).siblings('.accordionContent').slideToggle();
-		$(this).toggleClass('contentOpen');
-	});
-
-
-
-
 });
+
+//
+// $(document).bind('keydown', function(e) {
+//
+//   var contentAbout = $('#content-about').html();
+//   var contentCalendar = $('#content-calendar').html();
+//   var contentVisit = $('#content-visit').html();
+//   var contentGuestbook = $('#content-guestbook').html();
+//   var contentHelp = $('#content-help').html();
+//   var contentPrint = $('#content-print').html();
+//
+//   if (e.keyCode == 72) {
+//       // press the letter H
+//       $(".terminal").append(contentAbout);
+//   } else if (e.keyCode == 74) {
+//       //press the letter J
+//       $(".terminal").append(contentVisit);
+//   }
+//   return false;
+// });​
