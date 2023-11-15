b750464a933fe17e176ee92140d28db2319ba9d3
zai <zai@zais-mbp-2.myfiosgateway.com>
2023-11-15 18:08:51 -0500

    donate

--- public/js/fundraiser.js
+++ public/js/fundraiser.js
@@ -1,8 +1,8 @@
 //goal of the fundraiser, currently $60,000
-var goal = 60000;
+var goal = 40000;
 
 // estimated amount raised
-var raised = 10500;
+var raised = 0;
 
 // end date of fundraiser
 var end = new Date('January 3, 2023');
